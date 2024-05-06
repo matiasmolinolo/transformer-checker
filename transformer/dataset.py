b4f09565c2ffda605d0b27ff459ffdc86d305503
Matias Molinolo <matias@getpuppeteer.ai>
2024-05-06 16:28:22 -0300

    training 1st transformer

--- transformer/dataset.py
+++ transformer/dataset.py
@@ -1,7 +1,9 @@
+import json
 from typing import List
+
 import torch
 from torch.utils.data import Dataset
-import json
+
 
 class DyckLanguageTokenizer:
     START_TOKEN, PAD_TOKEN, END_TOKEN = 0, 1, 2
@@ -9,15 +11,18 @@ class DyckLanguageTokenizer:
 
     def __init__(self, vocab: str):
         self.vocab = vocab
-        self.tok_to_i = {**{tok: i + 3 for i, tok in enumerate(vocab)}, **self.base_vocab}
+        self.tok_to_i = {
+            **{tok: i + 3 for i, tok in enumerate(vocab)},
+            **self.base_vocab,
+        }
         self.i_to_tok = {i: tok for tok, i in self.tok_to_i.items()}
 
-    def tokenize(self, strings: List[str], max_len = None):
+    def tokenize(self, strings: List[str], max_len=None):
         def c_to_i(c):
             if c in self.tok_to_i:
-                return self.tok_to_i[c]
+                return float(self.tok_to_i[c])
             raise ValueError(f"Character {c} not in vocabulary")
-        
+
         if isinstance(strings, str):
             strings = [strings]
 
@@ -25,42 +30,52 @@ class DyckLanguageTokenizer:
             max_len = max((max(len(s) for s in strings)), 1)
 
         tokenized = [
-            [self.START_TOKEN] + [c_to_i(c) for c in s] + [self.END_TOKEN] + [self.PAD_TOKEN] * (max_len - len(s))
+            [self.START_TOKEN]
+            + [c_to_i(c) for c in s]
+            + [self.END_TOKEN]
+            + [self.PAD_TOKEN] * (max_len - len(s))
             for s in strings
         ]
 
-        return torch.tensor(tokenized)
-    
+        return torch.tensor(tokenized, dtype=torch.float)
+
     def decode(self, tokens):
         if tokens.ndim < 2:
             raise ValueError("Needs to have a batch dimension.")
-        
+
         def i_to_c(i):
             if i < len(self.i_to_tok):
                 return self.i_to_tok[i]
             raise ValueError(f"Index {i} not in vocabulary")
-        
+
         return [
-            "".join(i_to_c(i.item()) for i in seq[1:] if i != self.PAD_TOKEN and i != self.END_TOKEN)
+            "".join(
+                i_to_c(i.item())
+                for i in seq[1:]
+                if i != self.PAD_TOKEN and i != self.END_TOKEN
+            )
             for seq in tokens
         ]
-    
+
     def __repr__(self):
         return f"DyckLanguageTokenizer(vocab={self.vocab!r})"
+
 
 class DyckLanguageDataset(Dataset):
     def __init__(self, file, vocab):
         self.vocab = vocab
         self.tokenizer = DyckLanguageTokenizer(vocab)
         self.data = []
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             for line in f:
                 self.data.append(json.loads(line))
             print(f"Loaded {len(self.data)} samples from {file}")
-        
+
         self.strings = [sample[0] for sample in self.data]
         self.tokenized = self.tokenizer.tokenize(self.strings)
-        self.balanced = torch.tensor([sample[1] for sample in self.data])
+        self.balanced = torch.tensor(
+            [sample[1] for sample in self.data], dtype=torch.float
+        )
 
     def to(self, device):
         self.tokenized = self.tokenized.to(device)
@@ -69,12 +84,9 @@ class DyckLanguageDataset(Dataset):
 
     def __len__(self):
         return len(self.strings)
-    
+
     def __getitem__(self, idx):
         if type(idx) == slice:
             return self.__class__(list(zip(self.strings[idx], self.balanced[idx])))
 
         return (self.strings[idx], self.balanced[idx], self.tokenized[idx])
-    
-
-    
